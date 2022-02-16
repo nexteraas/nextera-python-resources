f13982160b4ae7aa3ce2f51f9a3103f8d1edb205
rsneumann <rsneumann@nextera.no>
2022-02-16 18:07:59 +0100

    test

--- nextera_utils/docker_interop.py
+++ nextera_utils/docker_interop.py
@@ -1,111 +1,65 @@
-from _ast import stmt
-from builtins import property
-import time
+import csv
+
 import pandas as pd
-import sys
-import uuid
-import os
 
 
 class DockerInterop:
-    _ARGS_FN='args.txt'
-    _EXPORT_FN = 'export_filename'
-    _IMPORT_FN = 'import_filename'
-    _PROGRESS_FN = 'progress_filename'
-    _EXCEPTION_FN = 'exception_filename'
+    _INPUT_HEADER_PREFIX = "input_fn_"
+    _OUTPUT_HEADER_PREFIX = "output_fn_"
+    _ARGS_FN = "arguments.csv"
+    _EXCEPTION_FN = "exception.txt"
+    _PROGRESS_FN = "progress.txt"
+    _DATA_EXCHANGE_IN_DIR = "/data_exchange/in/"
+    _DATA_EXCHANGE_OUT_DIR = "/data_exchange/out/"
+    _DEBUG_DATA_EXCHANGE_IN_DIR = "C:/docker_data_exchange/in/"
+    _DEBUG_DATA_EXCHANGE_OUT_DIR = "C:/docker_data_exchange/out/"
+    _INSTANCE = None
 
-    def __init__(self):
-        self._data_exchange_folder="/data_exchange"
-        if not os.path.isdir(self._data_exchange_folder):
-            self._data_exchange_folder = "/mnt/c/temp/docker_interop"
-        if not os.path.isdir(self._data_exchange_folder):
-            self._data_exchange_folder = "C:/temp/docker_interop"
+    @staticmethod
+    def get_instance():
+        if DockerInterop._INSTANCE is None:
+            raise Exception('Not instantiated')
+        return DockerInterop._INSTANCE
 
-    def _get_data_exchange_folder_in(self):
-        out=os.path.join(self._data_exchange_folder, 'in')
-        return out
+    def __init__(self, args_fn, debug_key=None):
+        self._args = self.read_csv(args_fn)
+        self._debug_key = debug_key
+        DockerInterop._INSTANCE = self
 
-    def _get_data_exchange_folder_out(self):
-        out=os.path.join(self._data_exchange_folder, 'out')
-        return out
+    def get_debug_key(self):
+        return self._debug_key
 
-    def get_input_fn(self, fn):
-        return os.path.join(self._get_data_exchange_folder_in(), fn)
-
-    def get_output_fn(self, fn):
-        return os.path.join(self._get_data_exchange_folder_out(), fn)
-
-    def read_df(self, fn):
-        df = pd.read_csv(fn, sep='\t')
+    def read_csv(self, fn, index_col=None):
+        df = pd.read_csv(fn, sep='\t', index_col=index_col)
         return df
 
-    def get_args(self):
-        df =self.read_df(self.get_input_fn(DockerInterop._ARGS_FN))
-        out=df.to_dict(orient='records')
-        return out[0]
-    # def _parse_args(self):
-    #     args_fn = sys.argv[1]
-    #     out = pd.read_csv(args_fn, sep='\t')
-    #     return out
-    #
-    # def read_data(self, index=None, index_col=None):
-    #     data_fn = self.get_data_fn(index)
-    #     if data_fn is None: return None, None
-    #     out = pd.read_csv(data_fn, sep='\t', index_col=index_col)
-    #     return data_fn, out
-    #
-    # def get_data_fn(self, index=None):
-    #     s = DockerInterop._EXPORT_FN
-    #     if index is not None:
-    #         s = s + str(index)
-    #     if s in self._args.columns:
-    #         out = self._args[s][0]
-    #     else:
-    #         out = None
-    #     return out
-    #
-    # def write_progress(self, value, comment):
-    #     progress_fn = self._get_progress_fn()
-    #     data = {'value': [value], 'comment': [comment]}
-    #     prog_df = pd.DataFrame.from_dict(data)
-    #     prog_df.astype({'value': 'float'})
-    #     path = os.path.dirname(progress_fn)
-    #     tmp_fn = os.path.join(path, str(uuid.uuid4()))
-    #     prog_df.to_csv(tmp_fn, index=False, sep='\t')
-    #     for i in range(0, 10):
-    #         if os.path.exists(progress_fn):
-    #             try:
-    #                 time.sleep(1)
-    #                 os.remove(progress_fn)
-    #             except:
-    #                 pass
-    #     os.rename(tmp_fn, progress_fn)
-    #
-    # def write_data(self, df, index=None):
-    #     output_fn = self._get_output_fn(index=None)
-    #     df.to_csv(output_fn, index=False, sep='\t')
-    #
-    # def write_exception(self, msg):
-    #     exc_fn = self._get_exception_fn()
-    #     path = os.path.dirname(exc_fn)
-    #     tmp_fn = os.path.join(path, str(uuid.uuid4()))
-    #     f = open(tmp_fn, 'w')
-    #     f.write(str(msg))
-    #     f.close()
-    #     os.rename(tmp_fn, exc_fn)
-    #
-    # def _get_progress_fn(self):
-    #     out = self._args[DockerInterop._PROGRESS_FN][0]
-    #     return out
-    #
-    # def _get_exception_fn(self):
-    #     out = self._args[DockerInterop._EXCEPTION_FN][0]
-    #     return out
-    #
-    # def _get_output_fn(self, index=None):
-    #     s = DockerInterop._IMPORT_FN
-    #     if index is not None:
-    #         s = s + str(index)
-    #     out = self._args[s][0]
-    #     return out
+    def get_input_filenames(self):
+        columns = self._args.columns.to_list()
+        out = []
+        for col in columns:
+            if col.startswith(DockerInterop._INPUT_HEADER_PREFIX):
+                filename = self._args[col].iloc[0]
+                if not self._debug_key is None:
+                    filename = self._create_debug_fn(filename)
+                out.append(filename)
+        return out
 
+    def get_output_filenames(self):
+        columns = self._args.columns.to_list()
+        out=[]
+        for col in columns:
+            if col.startswith(DockerInterop._OUTPUT_HEADER_PREFIX):
+                filename = self._args[col].iloc[0]
+                if not self._debug_key is None:
+                    filename = self._create_debug_fn(filename)
+                out.append(filename)
+        return out
+
+    def _create_debug_fn(self, fn):
+        if fn.startswith(DockerInterop._DATA_EXCHANGE_IN_DIR):
+            path = DockerInterop._DEBUG_DATA_EXCHANGE_IN_DIR + self._debug_key + '/'
+            out = fn.replace(DockerInterop._DATA_EXCHANGE_IN_DIR, path)
+        elif fn.startswith(DockerInterop._DATA_EXCHANGE_OUT_DIR):
+            path = DockerInterop._DEBUG_DATA_EXCHANGE_OUT_DIR + self._debug_key + '/'
+            out = fn.replace(DockerInterop._DATA_EXCHANGE_OUT_DIR, path)
+        return out
